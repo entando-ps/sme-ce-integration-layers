ffebad82ad53785d93b9589eeb3b0b3c0f0abfaf
GermanoGiudici <germano.giudici@gmail.com>
2022-09-25 15:32:27 +0200

    first commit

---
+++ src/main/java/com/entando/sme/cartaesercito/smeceintegrationlayers/SmeCeIntegrationLayersApplication.java
@@ -0,0 +1,13 @@
+package com.entando.sme.cartaesercito.smeceintegrationlayers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmeCeIntegrationLayersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmeCeIntegrationLayersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/entando/sme/cartaesercito/smeceintegrationlayers/SmeCeIntegrationLayersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entando.sme.cartaesercito.smeceintegrationlayers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmeCeIntegrationLayersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
