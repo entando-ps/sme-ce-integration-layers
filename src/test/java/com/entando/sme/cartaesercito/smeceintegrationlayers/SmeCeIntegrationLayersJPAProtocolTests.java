7b1f7cf3acd8730e078c6aad32102496c98048d7
GermanoGiudici <germano.giudici@gmail.com>
2022-09-27 12:46:52 +0200

    test rifattorizzato

--- src/test/java/com/entando/sme/cartaesercito/smeceintegrationlayers/SmeCeIntegrationLayersJPAProtocolTests.java
+++ src/test/java/com/entando/sme/cartaesercito/smeceintegrationlayers/SmeCeIntegrationLayersJPAProtocolTests.java
@@ -5,23 +5,27 @@ import com.entando.sme.cartaesercito.smeceintegrationlayers.repositories.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.util.Pair;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static com.entando.sme.cartaesercito.smeceintegrationlayers.Utils.parseString;
-
 @SpringBootTest
 class SmeCeIntegrationLayersJPAProtocolTests {
 
+    String csvSponsor = "/Users/germano/projects/SME/sme-ce-integration-layers/files/sponsor.csv";
+    String csvNucleoPrincipale = "/Users/germano/projects/SME/sme-ce-integration-layers/files/nucleoprincipale.csv";
+    String csvResidenzaSponsor = "/Users/germano/projects/SME/sme-ce-integration-layers/files/residenzasponsor.csv";
+    String csvResidenzeNucleoPrincipale = "/Users/germano/projects/SME/sme-ce-integration-layers/files/residenzaparentinucleoprincipale.csv";
+    String csvIstanzaNucleoPrincipale = "/Users/germano/projects/SME/sme-ce-integration-layers/files/istanzanucleoprincipale.csv";
+    String csvMandatoPagamento = "/Users/germano/projects/SME/sme-ce-integration-layers/files/mandatopagamento.csv";
+    String csvMandatoPagamentoPVC = "/Users/germano/projects/SME/sme-ce-integration-layers/files/mandatopagamentopvc.csv";
 
     @Autowired
     TabsoggettoJPARepository tabsoggettoJPARepository;
@@ -43,22 +47,62 @@ class SmeCeIntegrationLayersJPAProtocolTests {
     @Rollback(value = false)
     public void istanzaNucleoPrincipaleConNuoviSoggetti() {
 
-        String csvSponsor = "/Users/germano/projects/SME/sme-ce-integration-layers/files/sponsor.csv";
-        String csvNucleoPrincipale = "/Users/germano/projects/SME/sme-ce-integration-layers/files/nucleoprincipale.csv";
-        String csvResidenzaSponsor = "/Users/germano/projects/SME/sme-ce-integration-layers/files/residenzasponsor.csv";
-        String csvResidenzeNucleoPrincipale = "/Users/germano/projects/SME/sme-ce-integration-layers/files/residenzaparentinucleoprincipale.csv";
-        String csvIstanzaNucleoPrincipale = "/Users/germano/projects/SME/sme-ce-integration-layers/files/istanzanucleoprincipale.csv";
-        String csvMandatoPagamento = "/Users/germano/projects/SME/sme-ce-integration-layers/files/mandatopagamento.csv";
-        String csvMandatoPagamentoPVC = "/Users/germano/projects/SME/sme-ce-integration-layers/files/mandatopagamentopvc.csv";
 
+        //creazione dei soggetti
+        Pair<Tabsoggetto, List<Tabsoggetto>> tabsoggettoPair = insertSoggetti(csvSponsor, csvNucleoPrincipale);
+        Tabsoggetto sponsor = tabsoggettoPair.getFirst();
+        List<Tabsoggetto> listaSoggettiDelNucleoPrincipale = tabsoggettoPair.getSecond();
+
+        //aggancio delle residenze
+        insertResidenze(
+                sponsor,
+                csvResidenzaSponsor,
+                listaSoggettiDelNucleoPrincipale,
+                csvResidenzeNucleoPrincipale
+        );
+
+        //creazione dell'istanza di creazione delle carte esercito per un nuovo nucleo familiare
+        // e aggancio dei soggetti all'istanza
+        Tabistanza tabistanza = insertIstanza(
+                csvIstanzaNucleoPrincipale,
+                sponsor,
+                listaSoggettiDelNucleoPrincipale
+        );
+
+        //creazione del nucleo familiare
+        Tabnucleifull tabnucleifullSponsor = insertNucleoFull(
+                sponsor,
+                listaSoggettiDelNucleoPrincipale,
+                tabistanza
+        );
+
+        //inserimento dei mandati di pagamento
+        insertMandati(
+                sponsor,
+                tabnucleifullSponsor,
+                csvMandatoPagamento,
+                csvMandatoPagamentoPVC
+        );
+
+    }
+
+
+    public Pair<Tabsoggetto, List<Tabsoggetto>> insertSoggetti(String csvSponsor, String csvSoggettiNucleoPrincipale) {
+        //creazione sponsor
         List<String[]> sponsorProperties = readCsv(csvSponsor);
         Tabsoggetto sponsor = new Tabsoggetto(sponsorProperties.get(0));
-        List<String[]> nucleoPrincipale = readCsv(csvNucleoPrincipale);
+
+        //creazione sponsor nucleo
+        List<String[]> nucleoPrincipale = readCsv(csvSoggettiNucleoPrincipale);
         List<Tabsoggetto> parentinucleoprincipale = nucleoPrincipale.stream().map(Tabsoggetto::new).collect(Collectors.toList());
 
         tabsoggettoJPARepository.save(sponsor);
         tabsoggettoJPARepository.saveAll(parentinucleoprincipale);
+        return Pair.of(sponsor, parentinucleoprincipale);
 
+    }
+
+    public void insertResidenze(Tabsoggetto sponsor, String csvResidenzaSponsor, List<Tabsoggetto> parentinucleoprincipale, String csvResidenzeNucleoPrincipale) {
         Tabresidenze residenzaSponsor = new Tabresidenze(readCsv(csvResidenzaSponsor).get(0));
         residenzaSponsor.setRifSoggetto(sponsor.getIdSoggetto());
         tabresidenzeJPARepository.save(residenzaSponsor);
@@ -72,16 +116,22 @@ class SmeCeIntegrationLayersJPAProtocolTests {
 
         tabresidenzeJPARepository.saveAll(residenzeParentiNucleoPrincipale);
 
+    }
+
+    public Tabistanza insertIstanza(String csvIstanzaNucleoPrincipale, Tabsoggetto sponsor, List<Tabsoggetto> parentinucleoprincipale) {
         Tabistanza tabistanza = new Tabistanza(readCsv(csvIstanzaNucleoPrincipale).get(0));
         tabistanza.setIdSponsor(sponsor.getIdSoggetto());
         tabistanzaJPARepository.save(tabistanza);
-
         Tabsoggettiistanze tabsoggettiistanzeSponsor = new Tabsoggettiistanze(new TabsoggettiistanzePK(tabistanza.getIdIstanza(), sponsor.getIdSoggetto()));
-
         List<Tabsoggettiistanze> tabsoggettiistanzeList = parentinucleoprincipale.stream().map(tabsoggettoNucleoFamiliare -> new Tabsoggettiistanze(new TabsoggettiistanzePK(tabistanza.getIdIstanza(), tabsoggettoNucleoFamiliare.getIdSoggetto()))).collect(Collectors.toList());
         tabsoggettiistanzeList.add(tabsoggettiistanzeSponsor);
         tabsoggettiistanzeJPARepository.saveAll(tabsoggettiistanzeList);
+        return tabistanza;
 
+    }
+
+
+    public Tabnucleifull insertNucleoFull(Tabsoggetto sponsor, List<Tabsoggetto> parentinucleoprincipale, Tabistanza tabistanza) {
         Tabnucleifull tabnucleifullSponsor = new Tabnucleifull(true, true, tabistanza.getIdIstanza(), -1, sponsor.getIdSoggetto());
         tabnucleifullJPARepository.save(tabnucleifullSponsor);
         tabnucleifullSponsor.setRifNucleo(tabnucleifullSponsor.getId());
@@ -91,7 +141,10 @@ class SmeCeIntegrationLayersJPAProtocolTests {
         tabNucleiFullParenti.add(tabnucleifullSponsor);
         tabnucleifullJPARepository.saveAll(tabNucleiFullParenti);
 
+        return tabnucleifullSponsor;
+    }
 
+    public void insertMandati(Tabsoggetto sponsor, Tabnucleifull tabnucleifullSponsor, String csvMandatoPagamento, String csvMandatoPagamentoPVC) {
         Tabmandato tabmandato = new Tabmandato(readCsv(csvMandatoPagamento).get(0));
         tabmandato.setRifSponsor(sponsor.getIdSoggetto());
         tabmandato.setRifNucleofull(tabnucleifullSponsor.getId());
@@ -104,9 +157,7 @@ class SmeCeIntegrationLayersJPAProtocolTests {
         tabmandatoJPARepository.save(tabmandato);
         tabmandatopvcJPARepository.save(tabmandatopvc);
 
-
     }
-
 
     public List<String[]> readCsv(String csvFile) {
         try {
