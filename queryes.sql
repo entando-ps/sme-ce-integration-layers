e670ebee6b8a7f0da1a5252565d95dfb53c1068e
GermanoGiudici <germano.giudici@gmail.com>
2022-09-27 12:46:25 +0200

    nuove query per cancellazione

--- queryes.sql
+++ queryes.sql
@@ -18,14 +18,15 @@ select * from tabMandatoPVC
     join tabnucleifull on tabMandatoPVC.rifNucleoFull=tabnucleifull.id
     join tabsoggetto on tabMandatoPVC.rifSponsor=tabsoggetto.idSoggetto;
 
-delete from tabsoggetto;
+select idSoggetto from tabsoggetto where codiceFiscale in ('GDFGMN70D16H501T','GDLGMN70D16H501R','GDLGMN70D18H501F','GDLGMN70D18H501H');
 
-delete from tabresidenze;
 
-delete from tabistanza;
+delete from tabresidenze where rif_soggetto in (select idSoggetto from tabsoggetto where codiceFiscale in ('GDFGMN70D16H501T','GDLGMN70D16H501R','GDLGMN70D18H501F','GDLGMN70D18H501H'));
+delete from tabistanza where id_sponsor in (select idSoggetto from tabsoggetto where codiceFiscale in ('GDFGMN70D16H501T','GDLGMN70D16H501R','GDLGMN70D18H501F','GDLGMN70D18H501H'));
+delete from tabsoggettiistanze where rif_soggetto in (select idSoggetto from tabsoggetto where codiceFiscale in ('GDFGMN70D16H501T','GDLGMN70D16H501R','GDLGMN70D18H501F','GDLGMN70D18H501H'));
+delete from tabnucleifull where rif_soggetto in (select idSoggetto from tabsoggetto where codiceFiscale in ('GDFGMN70D16H501T','GDLGMN70D16H501R','GDLGMN70D18H501F','GDLGMN70D18H501H'));
+delete from tabmandato where rif_sponsor in (select idSoggetto from tabsoggetto where codiceFiscale in ('GDFGMN70D16H501T','GDLGMN70D16H501R','GDLGMN70D18H501F','GDLGMN70D18H501H'));;
+delete from tabMandatoPVC where rifSponsor in (select idSoggetto from tabsoggetto where codiceFiscale in ('GDFGMN70D16H501T','GDLGMN70D16H501R','GDLGMN70D18H501F','GDLGMN70D18H501H'));;
 
-delete from tabsoggettiistanze;
 
-delete from tabnucleifull;
-
-delete from tabmandato;
+delete from tabsoggetto where codiceFiscale in  ('GDFGMN70D16H501T','GDLGMN70D16H501R','GDLGMN70D18H501F','GDLGMN70D18H501H');
