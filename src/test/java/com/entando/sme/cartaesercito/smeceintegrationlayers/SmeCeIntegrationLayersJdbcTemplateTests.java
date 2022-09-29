3b6ecee287be1a7458ccff928532d46346a03d93
GermanoGiudici <germano.giudici@gmail.com>
2022-09-29 09:20:39 +0200

    new query

--- src/test/java/com/entando/sme/cartaesercito/smeceintegrationlayers/SmeCeIntegrationLayersJdbcTemplateTests.java
+++ src/test/java/com/entando/sme/cartaesercito/smeceintegrationlayers/SmeCeIntegrationLayersJdbcTemplateTests.java
@@ -1,43 +1,50 @@
 package com.entando.sme.cartaesercito.smeceintegrationlayers;
 
 import com.entando.sme.cartaesercito.smeceintegrationlayers.repositories.projections.SoggettoNucleoFamiliareDTOView;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Stream;
 
 @SpringBootTest
 class SmeCeIntegrationLayersJdbcTemplateTests {
 
 
+    @Value("${app.query.nucleoFamiliarePrincipaleSponsor}")
+    String nucleoFamiliarePrincipaleSponsorQuery;
+    @Value("${app.query.nucleoFamiliareEsternoSponsor}")
+    String nucleoFamiliareEsternoSponsorQuery;
+
     @Autowired
     NamedParameterJdbcTemplate jdbcTemplate;
 
 
     @Test
-    void query() throws IOException {
+    void nucleoFamiliarePrincipaleSponsor() throws IOException {
         SqlParameterSource namedParameters = new MapSqlParameterSource().addValue("codiceFiscale", "GDFGMN70D16H501T");
-        Stream<SoggettoNucleoFamiliareDTOView> soggettoNucleoFamiliareDTOViewStream = jdbcTemplate.queryForStream("select * from tabnucleifull " +
-                "join tabsoggetto on tabnucleifull.rif_soggetto=tabsoggetto.idSoggetto " +
-                "where rif_istanza = (select idIstanza from tabistanza " +
-                "where id_sponsor=(select idSoggetto from tabsoggetto where codiceFiscale=:codiceFiscale) and rif_tipoIstanza in (1, 3) ORDER BY idIstanza DESC LIMIT 1); ", namedParameters, new RowMapper<SoggettoNucleoFamiliareDTOView>() {
+        Stream<SoggettoNucleoFamiliareDTOView> soggettoNucleoFamiliareDTOViewStream = jdbcTemplate.queryForStream(nucleoFamiliarePrincipaleSponsorQuery, namedParameters, new RowMapper<SoggettoNucleoFamiliareDTOView>() {
+            @Override
+            public SoggettoNucleoFamiliareDTOView mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return new SoggettoNucleoFamiliareDTOView(rs);
+            }
+        });
+
+        soggettoNucleoFamiliareDTOViewStream.forEach(soggettoNucleoFamiliareDTOView -> System.out.println("soggettoNucleoFamiliareDTOView = " + soggettoNucleoFamiliareDTOView));
+
+    }
+    @Test
+    void nucleoFamiliareEsternoSponsor() throws IOException {
+        SqlParameterSource namedParameters = new MapSqlParameterSource().addValue("codiceFiscale", "GDFGMN70D16H501T");
+        Stream<SoggettoNucleoFamiliareDTOView> soggettoNucleoFamiliareDTOViewStream = jdbcTemplate.queryForStream(nucleoFamiliareEsternoSponsorQuery, namedParameters, new RowMapper<SoggettoNucleoFamiliareDTOView>() {
             @Override
             public SoggettoNucleoFamiliareDTOView mapRow(ResultSet rs, int rowNum) throws SQLException {
                 return new SoggettoNucleoFamiliareDTOView(rs);
