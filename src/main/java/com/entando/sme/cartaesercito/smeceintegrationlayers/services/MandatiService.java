6381e064551c11bb26adc68d1abeaaebe335b4e9
Emanuele Trisi <emanuele.trisi@gmail.com>
2023-02-17 15:41:38 +0100

    WIP aggiunto mandatiService per update mandato

---
+++ src/main/java/com/entando/sme/cartaesercito/smeceintegrationlayers/services/MandatiService.java
@@ -0,0 +1,79 @@
+package com.entando.sme.cartaesercito.smeceintegrationlayers.services;
+
+import com.entando.sme.cartaesercito.smeceintegrationlayers.config.ConfigurationParameters;
+import com.entando.sme.cartaesercito.smeceintegrationlayers.entities.Tabmandato;
+import com.entando.sme.cartaesercito.smeceintegrationlayers.entities.Tabmandatopvc;
+import com.entando.sme.cartaesercito.smeceintegrationlayers.repositories.TabmandatoJPARepository;
+import com.entando.sme.cartaesercito.smeceintegrationlayers.repositories.TabmandatopvcJPARepository;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class MandatiService {
+
+    final private TabmandatoJPARepository tabmandatoJPARepository;
+    final private TabmandatopvcJPARepository tabmandatopvcJPARepository;
+
+    public MandatiService(TabmandatoJPARepository tabmandatoJPARepository, TabmandatopvcJPARepository tabmandatopvcJPARepository, ConfigurationParameters configParam) {
+        this.tabmandatoJPARepository = tabmandatoJPARepository;
+        this.tabmandatopvcJPARepository = tabmandatopvcJPARepository;
+    }
+
+    /**metodo che passato l'id del mandato ed il cro aggiorna tabMandato e tabMandato pvc
+     *
+     *  !!! da rieseguire per ogni nucleo !!!
+     *
+     * @param codiceMandato id del mandato da aggiornare
+     * @param codiceMandatoPVC id del mandato pvc da aggiornare
+     * @param croMandato codice ricevuto da UI che attesta il pagamento mandato
+     * @param croMandatoPVC codice ricevuto da UI che attesta il pagamento mandato pvc
+     */
+    public void aggiornaMandatiConCRO(Integer codiceMandato, Integer codiceMandatoPVC, String croMandato, String croMandatoPVC){
+        // update su tabMandato con controllo se cro esiste
+        aggiornaMandatoConCRO(codiceMandato, croMandato);
+
+        // update su tabMandatoPVC con controllo se cro esiste
+        aggiornaMandatoPVCConCRO(codiceMandatoPVC, croMandatoPVC);
+    }
+
+    /**
+     * metodo che passato l'id del mandato ed il cro aggiorna tabMandato
+     *
+     * !!! da rieseguire per ogni mandato !!!
+     *
+     * @param codiceMandato id del mandato da aggiornare
+     * @param cro           codice ricevuto da UI che attesta il pagamento mandato
+     */
+    public void aggiornaMandatoConCRO(Integer codiceMandato, String cro) {
+        // cerca il mandato se non lo trova lancia NoSuchElementException
+        Tabmandato mandato = tabmandatoJPARepository.findById(codiceMandato).get();
+
+        // verifico se è già stato caricato un cro associato a quel mandato
+        if (mandato.getAttestazionePagamento() != null || mandato.getAttestazionePagamento().isBlank()) {
+            throw new UnsupportedOperationException("per il mandato con codice " + codiceMandato + " è già presente un'attestazione di pagamento");
+        }
+        mandato.setAttestazionePagamento(cro);
+        tabmandatoJPARepository.save(mandato);
+    }
+
+    /**
+     * metodo che passato l'id del mandato ed il cro aggiorna tabMandatoPvc
+     *
+     * !!! da rieseguire per ogni mandato pvc !!!
+     *
+     * @param codiceMandato id del mandato pvc da aggiornare
+     * @param cro           codice ricevuto da UI che attesta il pagamento mandato pvc
+     */
+    public void aggiornaMandatoPVCConCRO(Integer codiceMandato, String cro) {
+        // cerca il mandato se non lo trova lancia NoSuchElementException
+        Tabmandatopvc mandatoPVC = tabmandatopvcJPARepository.findById(codiceMandato).get();
+
+        // verifico se è già stato caricato un cro associato a quel mandato
+        if (mandatoPVC.getAttestazionePagamento() != null || !mandatoPVC.getAttestazionePagamento().isBlank()) {
+            throw new UnsupportedOperationException("per il mandato PVC con codice " + codiceMandato + " è già presente un'attestazione di pagamento");
+        }
+
+        mandatoPVC.setAttestazionePagamento(cro);
+        tabmandatopvcJPARepository.save(mandatoPVC);
+    }
+}
